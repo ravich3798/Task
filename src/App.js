6cc73c21f297bf7c539e333c2a025f2e10c7014f
thomas084 <56292903+thomas084@users.noreply.github.com>
2023-05-05 23:22:36 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup";
 
-function App() {
+  const schema=yup.object().shape({
+    firstName: yup.string().required("Only Strings Allowed!!!!"),
+    lastName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().required(),
+    password: yup.string().min(4).max(15).required(),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null])
+})
+function App(){
+    const {register, handleSubmit, errors}=useForm({
+        resolver: yupResolver(schema),
+    })
+    const submitForm=(data)=>{
+        console.log(data);
+    }
   return (
-    <div className="App">
-      <div>Hello World!!!!!</div>
-    </div>
-  );
+    <form onSubmit={handleSubmit(submitForm)}>
+        <input type="text" {...register("firstName")} />
+       
+        {/* <input type="text" name="lastName" placeholder="First Name" ref={register}/>
+        <input type="text" name="email" placeholder="First Name" ref={register}/>
+        <input type="text" name="age" placeholder="First Name" ref={register}/>
+        <input type="text" name="password" placeholder="First Name" ref={register}/>
+        <input type="text" name="confirmPassword" placeholder="First Name" ref={register}/> */}
+        
+        <input type="submit" name="Upload"/>
+        </form>
+    )
 }
 
 export default App;
